2ff2076a49fc3c0f78ddc829c730db4e1217ed95
KX3simsimsimPXR <sondosabd94@gmail.com>
2022-02-11 21:11:33 +0300

    first

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'account',
     'commerce',
 
-    'silk'
 ]
 
 MIDDLEWARE = [
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
