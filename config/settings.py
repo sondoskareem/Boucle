7aa6ab36c962d620e2890dd73b4c73a894ff0c8c
KX3simsimsimPXR <sondosabd94@gmail.com>
2022-02-11 21:26:53 +0300

    first

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-hlboh(#)k_md8ds(mjwkac5f4*+!52mh7xnu1645iucq%r9+vp'
-ALLOWED_HOSTS=['compute-django.herokuapp.com' , '127.0.0.1']
+ALLOWED_HOSTS=['*']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
